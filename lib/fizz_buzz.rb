3a07214dfd8585fade39e5cea4507f8cf1c0706a
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-06 14:40:46 +0200

    initializes Rspec

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,35 @@
+def fizz_buzz(number)
+    number
+        end
+
+def fizz_buzz(number)
+        if number % 15 == 0
+            'fizz buzz'
+        else
+            number
+        end
+    end
+
+def fizz_buzz(number)
+        if number % 3 == 0
+            'fizz'
+        else
+            number
+        end
+    end
+
+def fizz_buzz(number)
+        if number % 5 == 0
+            'buzz'
+        else
+            number
+        end
+    end
+
+# def has_zero_remainder? 
+ #       number % divider == 0
+  #      end
+    
+
+
+
