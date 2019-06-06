11122305b415791ae6a94afb53ca0cbce7fd3df9
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-06 16:07:49 +0200

    all fizzbuzz commit

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,35 +1,15 @@
 def fizz_buzz(number)
-    number
-        end
-
-def fizz_buzz(number)
-        if number % 15 == 0
-            'fizz buzz'
-        else
-            number
-        end
+    if number % 15 == 0
+        'fizz buzz'        
+    elsif number % 5 == 0
+        'buzz'
+    elsif number % 3 == 0
+        'fizz'
+    else
+        number
     end
+end
 
-def fizz_buzz(number)
-        if number % 3 == 0
-            'fizz'
-        else
-            number
-        end
-    end
-
-def fizz_buzz(number)
-        if number % 5 == 0
-            'buzz'
-        else
-            number
-        end
-    end
-
-# def has_zero_remainder? 
- #       number % divider == 0
-  #      end
-    
 
 
 
